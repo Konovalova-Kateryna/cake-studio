0e1aa46d3adcfb6f8a463ce8028b1da3a6bd1f0d
Konovalova-Kateryna <95272926+Konovalova-Kateryna@users.noreply.github.com>
2023-08-27 18:05:20 +0000

    Deploying to gh-pages from @ Konovalova-Kateryna/cake-studio@2d005264a2416c78f5aa67a9aa1538680f677d55 🚀

---
+++ index.50a4289a.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{ItcSlider.getOrCreateInstance(".itc-slider",{loop:!0,swipe:!0,refresh:!0,autoplay:!0,interval:6e3})}));
+//# sourceMappingURL=index.50a4289a.js.map
--- index.ba0c03e2.js
+++
@@ -1,2 +0,0 @@
-$(".slider").slick({slidesToShow:3,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3,arrows:!0,adaptiveHeight:!0,prevArrow:$(".prev-btn"),nextArrow:$(".next-btn")});
-//# sourceMappingURL=index.ba0c03e2.js.map
