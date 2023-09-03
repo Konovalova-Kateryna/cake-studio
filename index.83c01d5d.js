c433f9976c3f8a1ae8fe556e161394de13216b8d
Konovalova-Kateryna <95272926+Konovalova-Kateryna@users.noreply.github.com>
2023-09-03 20:47:50 +0000

    Deploying to gh-pages from @ Konovalova-Kateryna/cake-studio@859f4f69ea638627873902890f018fe457adc9b2 🚀

---
+++ index.83c01d5d.js
@@ -0,0 +1,2 @@
+function pleaseShow(){document.getElementById("trendBox").classList.toggle("show")}function current(){const e=document.getElementsByClassName("assortment__btn");console.log(e),e.addEventListener.classList.add("current")}
+//# sourceMappingURL=index.83c01d5d.js.map
